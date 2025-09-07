1e732e3505475241d369c4733f9f2bf935f417c1
MCRLogPose <cruzriveraedwinmanuel@gmail.com>
2025-09-07 15:28:31 -0500

    Segundo commit

---
+++ src/test/java/com/huerfanosSRL/Huerfanos_Back/HuerfanosBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huerfanosSRL.Huerfanos_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuerfanosBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
